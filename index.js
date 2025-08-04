d6fad384b0058da7161685b9c4ed70dba689d90b
M.Noman <153206686+Noman2218@users.noreply.github.com>
2025-08-04 18:05:51 +0500

    ---

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-express = require("express");
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:3000`));
